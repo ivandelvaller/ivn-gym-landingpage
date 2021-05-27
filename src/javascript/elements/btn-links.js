a393826337fa9d0f7fdc7587fa2c436ca4c816db
ivandelvalle <tec.ivandelvaller@outlook.com>
2021-05-27 01:10:28 -0500

    fixed linked

--- src/javascript/elements/btn-links.js
+++ src/javascript/elements/btn-links.js
@@ -14,7 +14,7 @@ $links.addEventListener('click', event => {
 
 	if (target.classList.contains('link')) {
 		const name = target.innerText.toLowerCase().replace(' ', '-')
-		window.location = window.origin + window.pathname + '#' + name
+		window.location = window.origin + '/ivn-gym-landingpage/#' + name
 
 		$btnNav.click()
 	}
