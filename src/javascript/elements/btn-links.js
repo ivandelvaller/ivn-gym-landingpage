8e91b74b081baa810d7baac0778a39f60ed8ebc6
ivandelvalle <tec.ivandelvaller@outlook.com>
2021-05-27 13:52:39 -0500

    fixed compatibility

--- src/javascript/elements/btn-links.js
+++ src/javascript/elements/btn-links.js
@@ -7,15 +7,13 @@ import {
 } from './btn-nav';
 
 const $links = document.getElementById('link-list')
-const $link = $links.querySelectorAll('.link')
 
 $links.addEventListener('click', event => {
-	const target = event.originalTarget
+	const target = event.target
 
 	if (target.classList.contains('link')) {
 		const name = target.innerText.toLowerCase().replace(' ', '-')
 		window.location.hash = name
-		console.log(window.location.hash)
 		event.preventDefault()
 		$btnNav.click()
 	}
